dfd458b6d87f185629a3811dad00245ec690743f
RAW-ON <agungwar00@gmail.com>
2016-11-30 13:09:40 +0700

    RecyclerView List

---
+++ app/src/main/java/id/sch/smktelkom_mlg/learn/recyclerview1/model/hotel.java
@@ -0,0 +1,19 @@
+package id.sch.smktelkom_mlg.learn.recyclerview1.model;
+
+import android.graphics.drawable.Drawable;
+
+/**
+ * Created by user on 02/11/2016.
+ */
+
+public class hotel {
+    public String judul;
+    public String deskripsi;
+    public Drawable foto;
+
+    public hotel(String judul, String deskripsi, Drawable foto) {
+        this.judul = judul;
+        this.deskripsi = deskripsi;
+        this.foto = foto;
+    }
+}
